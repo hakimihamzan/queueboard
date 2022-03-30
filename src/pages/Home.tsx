ee35f9ffde1d64c2ac45643ad48dc742275d26af
kimi <hakimihamzan20@gmail.com>
2022-03-30 16:10:18 +0800

    Added skeleton page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className='flex items-center justify-center w-screen h-screen'>Welcome to QueueBoard</div>
+    )
+}
+
+export default HomePage
